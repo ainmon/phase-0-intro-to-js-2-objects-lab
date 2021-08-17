0d4df7164a0c95d62910e493c27b74e062dd1a0f
Adam <inmon.adam@yahoo.com>
2021-08-16 21:42:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+var employee = {
+        name: "Adam",
+        streetAddress: "349 Cherry Ln",
+}
+
+function updateEmployeeWithKeyAndValue() {
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway";
+    return employee;
+
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee}; 
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+
+}
